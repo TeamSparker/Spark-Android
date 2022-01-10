8340421e636bd453b0d59c3eaaba69e15a450367
Yunjoo Son <thsduswn4141@naver.com>
2022-01-11 05:53:37 +0900

    #13 [layout] 프로필 입력 화면 디자인 적용

---
+++ app/src/main/java/com/spark/android/ui/profile/ProfileFragment.kt
@@ -0,0 +1,16 @@
+package com.spark.android.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.spark.android.R
+import com.spark.android.databinding.FragmentProfileBinding
+import com.spark.android.ui.base.BaseFragment
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
