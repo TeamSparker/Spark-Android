7cf6ed66971e138237a4d8fc46f120883f71d66c
Yunjoo Son <thsduswn4141@naver.com>
2022-01-11 17:00:31 +0900

    #13 [feat] SignInFragment에서 ProfileFragment로 화면이동 구현

--- app/src/main/java/com/spark/android/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/spark/android/ui/signin/SignInFragment.kt
@@ -9,14 +9,22 @@ import androidx.core.content.ContextCompat.getColor
 import com.spark.android.R
 import com.spark.android.databinding.FragmentSignInBinding
 import com.spark.android.ui.base.BaseFragment
+import com.spark.android.util.navigate
 
 class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setStatusBarColor()
+        initKakaoLoginBtnClickListener()
     }
 
     private fun setStatusBarColor() {
         requireActivity().window.statusBarColor = getColor(requireContext(), R.color.spark_pinkred)
+    }
+
+    private fun initKakaoLoginBtnClickListener() {
+        binding.btnSignInKakaoLogin.setOnClickListener {
+            navigate(R.id.action_signInFragment_to_profileFragment)
+        }
     }
 }
