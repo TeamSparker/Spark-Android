55649e33f5282e9642c3e6adf9618bdaa25ff55b
Jaehun-Jo <chojh922@gmail.com>
2022-01-11 19:47:47 +0900

    [modify] HabitActivity 추가, HabitFragment 삭제

---
+++ app/src/main/java/com/spark/android/ui/habit/HabitActivity.kt
@@ -0,0 +1,12 @@
+package com.spark.android.ui.habit
+
+import android.os.Bundle
+import com.spark.android.R
+import com.spark.android.databinding.ActivityHabitBinding
+import com.spark.android.ui.base.BaseActivity
+
+class HabitActivity : BaseActivity<ActivityHabitBinding>(R.layout.activity_habit) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
