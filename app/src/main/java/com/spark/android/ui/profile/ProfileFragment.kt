a668d7f148c947818e781a9489f3fcef37763233
Yunjoo Son <thsduswn4141@naver.com>
2022-01-11 19:02:12 +0900

    #13 [feat] StatusBarUtil 생성 후 적용 및 테마컬러 설정

--- app/src/main/java/com/spark/android/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/spark/android/ui/profile/ProfileFragment.kt
@@ -1,16 +1,17 @@
 package com.spark.android.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.spark.android.R
 import com.spark.android.databinding.FragmentProfileBinding
 import com.spark.android.ui.base.BaseFragment
+import com.spark.android.util.initStatusBarColor
+import com.spark.android.util.initStatusBarTextColorToWhite
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().initStatusBarColor(R.color.spark_white)
+        requireActivity().initStatusBarTextColorToWhite()
     }
 }
--- app/src/main/java/com/spark/android/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/spark/android/ui/signin/SignInFragment.kt
@@ -1,25 +1,20 @@
 package com.spark.android.ui.signin
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getColor
 import com.spark.android.R
 import com.spark.android.databinding.FragmentSignInBinding
 import com.spark.android.ui.base.BaseFragment
+import com.spark.android.util.initStatusBarColor
+import com.spark.android.util.initStatusBarTextColorToWhite
 import com.spark.android.util.navigate
 
 class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setStatusBarColor()
+        requireActivity().initStatusBarColor(R.color.spark_pinkred)
+        requireActivity().initStatusBarTextColorToWhite()
         initKakaoLoginBtnClickListener()
-    }
-
-    private fun setStatusBarColor() {
-        requireActivity().window.statusBarColor = getColor(requireContext(), R.color.spark_pinkred)
     }
 
     private fun initKakaoLoginBtnClickListener() {
