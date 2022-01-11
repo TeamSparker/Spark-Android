f2d66fcac7eb6b234fbc7a4d7364a4e66ae12b8a
Yunjoo Son <thsduswn4141@naver.com>
2022-01-11 21:15:21 +0900

    #13 [feat] KeyBoardUtil 생성 후 키보드로 입력 시 화면 영역 클릭하면 키보드 숨기기 구현

--- app/src/main/java/com/spark/android/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/spark/android/ui/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.spark.android.R
 import com.spark.android.databinding.FragmentProfileBinding
 import com.spark.android.ui.base.BaseFragment
+import com.spark.android.util.KeyBoardUtil
 import com.spark.android.util.initStatusBarColor
 import com.spark.android.util.initStatusBarTextColorToWhite
 
@@ -15,5 +16,12 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_p
         binding.profileViewModel = ProfileViewModel()
         requireActivity().initStatusBarColor(R.color.spark_white)
         requireActivity().initStatusBarTextColorToWhite()
+        hideKeyBoard()
+    }
+
+    private fun hideKeyBoard() {
+        binding.layoutProfile.setOnClickListener {
+            KeyBoardUtil.hide(requireActivity())
+        }
     }
 }
