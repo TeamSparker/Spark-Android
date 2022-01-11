54934e49a534f2b6036296152f10e845a95fd72c
Grandelwald <artjr97@naver.com>
2022-01-11 18:02:12 +0900

    #6 [feat] fragment_storage.xml의 텍스트뷰 누르면 화면 바뀌는 기능 구현

--- app/src/main/java/com/spark/android/ui/storage/StorageFragment.kt
+++ app/src/main/java/com/spark/android/ui/storage/StorageFragment.kt
@@ -1,26 +1,38 @@
 package com.spark.android.ui.storage
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.spark.android.R
 import com.spark.android.databinding.FragmentStorageBinding
 import com.spark.android.ui.base.BaseFragment
+import com.spark.android.ui.storage.viewmodel.StorageViewModel
+import java.util.Observer
 
 
 class StorageFragment : BaseFragment<FragmentStorageBinding>(R.layout.fragment_storage) {
     private lateinit var viewPagerOutAdapter: StorageViewPagerOutAdapter
+    private val storageViewModel: StorageViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.storageViewModel = storageViewModel
         initStorageOutAdapter()
+
+        storageViewModel.sparkMode.observe(viewLifecycleOwner) { mode ->
+            binding.vpStorageOut.currentItem = mode
+        }
 
     }
 
+
     private fun initStorageOutAdapter() {
-        val fragmentList = listOf(StorageProgressingFragment(),StorageCompleteFragment(),StorageIncompleteFragment())
+        val fragmentList = listOf(
+            StorageProgressingFragment(),
+            StorageCompleteFragment(),
+            StorageIncompleteFragment()
+        )
         viewPagerOutAdapter = StorageViewPagerOutAdapter(requireActivity())
         viewPagerOutAdapter.fragments.addAll(fragmentList)
 
--- app/src/main/java/com/spark/android/ui/storage/viewmodel/StorageViewModel.kt
+++ app/src/main/java/com/spark/android/ui/storage/viewmodel/StorageViewModel.kt
@@ -5,8 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StorageViewModel : ViewModel() {
-    private val _storagePage = MutableLiveData<Int>()
-    val storagePage : LiveData<Int> = _storagePage
+    private val _sparkMode = MutableLiveData(0)
+    val sparkMode: LiveData<Int> = _sparkMode
+
+    fun initProgressMode() {
+        _sparkMode.value = PROGRESSING
+    }
+
+    fun initCompleteMode() {
+        _sparkMode.value = COMPLETE
+    }
+
+    fun initIncompleteMode() {
+        _sparkMode.value = INCOMPLETE
+    }
 
     companion object {
         private const val PROGRESSING = 0
