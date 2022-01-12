c6b1f0d9fba652b5263f0498922652724acacdbf
Yunjoo Son <thsduswn4141@naver.com>
2022-01-12 17:32:36 +0900

    #15 [layout] FeedFragment 디자인 구현

---
+++ app/src/main/java/com/spark/android/ui/feed/FeedFragment.kt
@@ -0,0 +1,8 @@
+package com.spark.android.ui.feed
+
+import com.spark.android.R
+import com.spark.android.databinding.FragmentFeedBinding
+import com.spark.android.ui.base.BaseFragment
+
+class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
+}
