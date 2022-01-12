89f2aa303158508877a14bc878e6106d50651361
Jaehun-Jo <chojh922@gmail.com>
2022-01-12 14:45:23 +0900

    [feat] HabitRecyclerViewAdapter.kt 추가, HabitActivity.kt 어댑터 연결, activity_habit.xml SwipeRefreshLayout 추가 및 item_habit_team.xml 레이아웃 수정,

--- app/src/main/java/com/spark/android/ui/habit/HabitActivity.kt
+++ app/src/main/java/com/spark/android/ui/habit/HabitActivity.kt
@@ -4,9 +4,27 @@ import android.os.Bundle
 import com.spark.android.R
 import com.spark.android.databinding.ActivityHabitBinding
 import com.spark.android.ui.base.BaseActivity
+import com.spark.android.ui.habit.adapter.HabitRecyclerViewAdapter
 
 class HabitActivity : BaseActivity<ActivityHabitBinding>(R.layout.activity_habit) {
+    private lateinit var habitRecyclerViewAdapter : HabitRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initRVAdapter()
+    }
+
+    private fun initRVAdapter() {
+        // test
+        var list = mutableListOf<String>()
+        list.add("a")
+        list.add("b")
+        list.add("c")
+        list.add("d")
+        list.add("e")
+        list.add("f")
+        habitRecyclerViewAdapter = HabitRecyclerViewAdapter(list)
+        binding.rvHabitTeamList.adapter = habitRecyclerViewAdapter
     }
 }
