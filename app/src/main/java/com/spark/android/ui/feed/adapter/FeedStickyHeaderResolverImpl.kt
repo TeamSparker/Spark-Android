7a689e5280cec7a72840534b2126d92dfe771269
Yunjoo Son <thsduswn4141@naver.com>
2022-01-14 03:30:41 +0900

    #15 [feat] Feed Recyclerview 관련 모델 작성

---
+++ app/src/main/java/com/spark/android/ui/feed/adapter/FeedStickyHeaderResolverImpl.kt
@@ -0,0 +1,34 @@
+package com.spark.android.ui.feed.adapter
+
+import androidx.databinding.ViewDataBinding
+import com.spark.android.data.response.FeedListItem
+
+class FeedStickyHeaderResolverImpl(
+    private val items: List<FeedListItem>,
+    private val dateVarBind: Int,
+    private val dayVarBind: Int,
+    private val isHeader: FeedListItem.() -> Boolean
+) : StickyHeaderResolver {
+
+    override fun getItemHeaderForPos(idx: Int): FeedListItem {
+        var header: FeedListItem? = null
+        for ((i, item) in items.withIndex()) {
+            if (isHeader(item)) {
+                header = item
+            }
+            if (i == idx) {
+                break
+            }
+        }
+        return header ?: throw IllegalArgumentException("StickyHeaderProvider 에 Header 아이템 null 오류")
+    }
+
+
+    override fun ViewDataBinding.bindHeader(item: FeedListItem) {
+        setVariable(dateVarBind, item.headerDate)
+        setVariable(dayVarBind, item.headerDay)
+        executePendingBindings()
+    }
+
+    override fun isHeader(position: Int) = isHeader(items[position])
+}
