1dd856e72137f658667d53e6dc13dd3b34cb1c99
changhwan <chang5hustle@gmail.com>
2022-01-14 02:08:55 +0900

    #28 [feat] 바텀네비게이션 대체 완료(추후 데이터바인딩 리펙토링 필요)

--- app/src/main/java/com/spark/android/ui/MainActivity.kt
+++ app/src/main/java/com/spark/android/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.spark.android.ui
 
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.spark.android.R
+import com.spark.android.SparkApplication
 import com.spark.android.databinding.ActivityMainBinding
 import com.spark.android.ui.base.BaseActivity
 import com.spark.android.ui.home.FeedFragment
@@ -21,10 +24,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
         initStatusBarStyle()
         initTransactionEvent()
-        initBottomNavigation()
+        initBottomNavigationFeedButton()
+        initBottomNavigationHomeButton()
+        initBottomNavigationStorageButton()
+
     }
 
-    private fun initStatusBarStyle(){
+    private fun initStatusBarStyle() {
         initStatusBarColor(R.color.spark_white)
         initStatusBarTextColorToWhite()
     }
@@ -33,29 +39,43 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         feedFragment = FeedFragment()
         homeMainFragment = HomeMainFragment()
         storageFragment = StorageFragment()
-        supportFragmentManager.beginTransaction().add(R.id.container_main, homeMainFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.container_main, homeMainFragment)
+            .commit()
     }
 
-    private fun initBottomNavigation() {
-        binding.bottomNavMain.selectedItemId = R.id.menu_home
+    private fun initBottomNavigationFeedButton() {
+        binding.btnMainBottomNavFeed.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.container_main, feedFragment).commit()
+            binding.btnMainBottomNavFeed.setImageResource(R.drawable.ic_bottom_navigation_feed_selected)
+            binding.tvMainBottomNavFeed.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_pinkred))
+            binding.btnMainBottomNavHome.setImageResource(R.drawable.ic_bottom_navigation_home)
+            binding.tvMainBottomNavHome.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_gray))
+            binding.btnMainBottomNavStorage.setImageResource(R.drawable.ic_bottom_navigation_storage)
+            binding.tvMainBottomNavStorage.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_gray))
+        }
+    }
 
-        binding.bottomNavMain.setOnItemSelectedListener {
-            val transaction = supportFragmentManager.beginTransaction()
+    private fun initBottomNavigationHomeButton() {
+        binding.btnMainBottomNavHome.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.container_main, homeMainFragment).commit()
+            binding.btnMainBottomNavFeed.setImageResource(R.drawable.ic_bottom_navigation_feed)
+            binding.tvMainBottomNavFeed.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_gray))
+            binding.btnMainBottomNavHome.setImageResource(R.drawable.ic_bottom_navigation_home_selected)
+            binding.tvMainBottomNavHome.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_pinkred))
+            binding.btnMainBottomNavStorage.setImageResource(R.drawable.ic_bottom_navigation_storage)
+            binding.tvMainBottomNavStorage.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_gray))
+        }
+    }
 
-            when (it.itemId) {
-                R.id.menu_feed -> {
-                    transaction.replace(R.id.container_main, feedFragment).commit()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.menu_home -> {
-                    transaction.replace(R.id.container_main, homeMainFragment).commit()
-                    return@setOnItemSelectedListener true
-                }
-                else -> {
-                    transaction.replace(R.id.container_main, storageFragment).commit()
-                    return@setOnItemSelectedListener true
-                }
-            }
+    private fun initBottomNavigationStorageButton() {
+        binding.btnMainBottomNavStorage.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.container_main, storageFragment).commit()
+            binding.btnMainBottomNavFeed.setImageResource(R.drawable.ic_bottom_navigation_feed)
+            binding.tvMainBottomNavFeed.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_gray))
+            binding.btnMainBottomNavHome.setImageResource(R.drawable.ic_bottom_navigation_home)
+            binding.tvMainBottomNavHome.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_gray))
+            binding.btnMainBottomNavStorage.setImageResource(R.drawable.ic_bottom_navigation_storage_selected)
+            binding.tvMainBottomNavStorage.setTextColor(ContextCompat.getColor(SparkApplication.ApplicationContext(), R.color.spark_dark_pinkred))
         }
     }
 
