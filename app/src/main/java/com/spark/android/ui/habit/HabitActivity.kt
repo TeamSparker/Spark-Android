1893038037ef40b7bc512d43afd145b7bfcb0d6f
Jaehun-Jo <chojh922@gmail.com>
2022-01-14 13:11:14 +0900

    [feat] 더보기 BottomSheetDialog, 오늘의 인증 BottomSheetDialog 레이아웃 디자인 및 버튼 연결

--- app/src/main/java/com/spark/android/ui/habit/HabitActivity.kt
+++ app/src/main/java/com/spark/android/ui/habit/HabitActivity.kt
@@ -2,6 +2,9 @@ package com.spark.android.ui.habit
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.spark.android.R
 import com.spark.android.databinding.ActivityHabitBinding
@@ -11,45 +14,30 @@ import com.spark.android.util.initStatusBarColor
 
 class HabitActivity : BaseActivity<ActivityHabitBinding>(R.layout.activity_habit) {
     private lateinit var habitRecyclerViewAdapter: HabitRecyclerViewAdapter
-    var testFlag = true
+    private lateinit var moreBottomSheetDialog: BottomSheetDialog
+    private lateinit var todayBottomSheetDialog: BottomSheetDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initStatusBarColor(R.color.spark_black)
-        initRVAdapter()
         setSwipeRefreshLayout()
+        initRVAdapter()
+        initHabitMoreDialog()
+        initHabitTodayDialog()
         initHabitBackBtnClickListener()
-//        initHabitMoreBtnClickListener()
-        initCertificationBtnClickListener()
+        initHabitMoreBtnClickListener()
+        initHabitTodayBtnClickListener()
+        initHabitMoreManageBtnClickListener()
     }
 
-    private fun initHabitBackBtnClickListener() {
-        binding.btnHabitBack.setOnClickListener {
-            finish()
-        }
-    }
 
-//    private fun initHabitMoreBtnClickListener() {
-//        binding.btnHabitMore.setOnClickListener {
-//            val moreBottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_habit_more, null)
-////            val moreBottomSheetDialog = BottomSheetDialog(this, R.style.NewDialog)
-//            val moreBottomSheetDialog = BottomSheetDialog(this)
-//            moreBottomSheetDialog.setContentView(moreBottomSheetView)
-//            moreBottomSheetDialog.show()
-//        }
-//    }
-
-    private fun initCertificationBtnClickListener() {
-        binding.btnHabitTodayCertification.setOnClickListener {
-            if (testFlag) {
-                binding.btnHabitTodayCertification.setBackgroundResource(R.drawable.bg_habit_today_inactive)
-                binding.ivHabitTodayCertificationBg.visibility = View.GONE
-                testFlag = !testFlag
-            } else {
-                binding.btnHabitTodayCertification.setBackgroundResource(R.drawable.bg_habit_today_active)
-                binding.ivHabitTodayCertificationBg.visibility = View.VISIBLE
-                testFlag = !testFlag
+    private fun setSwipeRefreshLayout() {
+        with(binding.swipeHabitRefresh) {
+            setColorSchemeColors(context.getColor(R.color.spark_pinkred))
+            setOnRefreshListener {
+                // 함수 추가
+                isRefreshing = false
             }
         }
     }
@@ -67,13 +55,43 @@ class HabitActivity : BaseActivity<ActivityHabitBinding>(R.layout.activity_habit
         binding.rvHabitTeamList.adapter = habitRecyclerViewAdapter
     }
 
-    private fun setSwipeRefreshLayout() {
-        with(binding.swipeHabitRefresh) {
-            setColorSchemeColors(context.getColor(R.color.spark_pinkred))
-            setOnRefreshListener {
-                // 함수 추가
-                isRefreshing = false
-            }
+    private fun initHabitMoreDialog() {
+        val moreBottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_habit_more, null)
+        moreBottomSheetDialog = BottomSheetDialog(this, R.style.NewDialog)
+//            val moreBottomSheetDialog = BottomSheetDialog(this)
+        moreBottomSheetDialog.setContentView(moreBottomSheetView)
+    }
+
+    private fun initHabitTodayDialog() {
+        val todayBottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_habit_today, null)
+        todayBottomSheetDialog = BottomSheetDialog(this, R.style.NewDialog)
+        todayBottomSheetDialog.setContentView(todayBottomSheetView)
+    }
+
+    private fun initHabitBackBtnClickListener() {
+        binding.btnHabitBack.setOnClickListener {
+            finish()
         }
+    }
+
+    private fun initHabitMoreBtnClickListener() {
+        binding.btnHabitMore.setOnClickListener {
+            moreBottomSheetDialog.show()
+        }
+    }
+
+    private fun initHabitTodayBtnClickListener() {
+        binding.btnHabitTodayCertification.setOnClickListener {
+            todayBottomSheetDialog.findViewById<ImageView>(R.id.iv_habit_today_stopwatch)?.visibility =
+                View.GONE
+            todayBottomSheetDialog.show()
+        }
+    }
+
+    private fun initHabitMoreManageBtnClickListener() {
+        moreBottomSheetDialog.findViewById<TextView>(R.id.tv_habit_more_manage)
+            ?.setOnClickListener {
+                Toast.makeText(this, "manage", Toast.LENGTH_SHORT).show()
+            }
     }
 }
