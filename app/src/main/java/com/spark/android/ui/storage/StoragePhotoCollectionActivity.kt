933f574a229427ef928dbda6d39d3156bfdb3345
Grandelwald <artjr97@naver.com>
2022-01-15 16:29:09 +0900

    #32 [chore] 인증사진 리스트 더미데이터 갯수 늘림

--- app/src/main/java/com/spark/android/ui/storage/StoragePhotoCollectionActivity.kt
+++ app/src/main/java/com/spark/android/ui/storage/StoragePhotoCollectionActivity.kt
@@ -11,11 +11,34 @@ class StoragePhotoCollectionActivity :
     private val photoCollectionRvAdapter = PhotoCollectionRvAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding.ivStoragePhotoCollectionBackWhite.setOnClickListener {
+            finish()
+        }
         initStoragePhotoCollectionRvAdapter()
     }
 
     private fun initStoragePhotoCollectionRvAdapter() {
-        photoCollectionRvAdapter.setList(listOf("D-day", "D-1", "D-2", "D-3", "D-4", "D-5"))
+        photoCollectionRvAdapter.setList(
+            listOf(
+                "D-day",
+                "D-1",
+                "D-2",
+                "D-3",
+                "D-4",
+                "D-5",
+                "D-6",
+                "D-7",
+                "D-8",
+                "D-9",
+                "D-10",
+                "D-11",
+                "D-12",
+                "D-13",
+                "D-14",
+                "D-15"
+            )
+        )
+        binding.rvStoragePhotoCollection.adapter = photoCollectionRvAdapter
     }
+
 }
