01a711db42b5f33d9f6072e005305ce828c8b23b
Yunjoo Son <thsduswn4141@naver.com>
2022-01-15 21:02:28 +0900

    #49 [refactor] SparkMessagingService 리팩토링

--- app/src/main/java/com/spark/android/SparkMessagingService.kt
+++ app/src/main/java/com/spark/android/SparkMessagingService.kt
@@ -18,27 +18,30 @@ class SparkMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         if (remoteMessage.data.isNotEmpty()) {
-            val alarmId = remoteMessage.sentTime.toInt()
-            val intent = Intent(this, IntroActivity::class.java)
-            val pendingIntent =
-                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-            val builder =
-                NotificationCompat.Builder(this, getString(R.string.app_name))
-                    .setContentTitle(remoteMessage.data["title"].toString())
-                    .setContentText(remoteMessage.data["body"].toString())
-                    .setSmallIcon(R.mipmap.ic_app_logo)
-                    .setContentIntent(pendingIntent)
-                    .setPriority(NotificationCompat.PRIORITY_MAX)
-                    .setAutoCancel(true)
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val channelId = getString(R.string.app_name)
-            val channelName = getString(R.string.app_name)
-            val channelImportance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(channelId, channelName, channelImportance)
-            notificationManager.createNotificationChannel(channel)
-
-            notificationManager.notify(alarmId, builder.build())
+            showNotification(remoteMessage)
         }
+    }
+
+    private fun showNotification(remoteMessage: RemoteMessage) {
+        val alarmId = remoteMessage.sentTime.toInt()
+        val intent = Intent(this, IntroActivity::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val builder =
+            NotificationCompat.Builder(this, getString(R.string.app_name))
+                .setContentTitle(remoteMessage.data["title"].toString())
+                .setContentText(remoteMessage.data["body"].toString())
+                .setSmallIcon(R.mipmap.ic_app_logo)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setAutoCancel(true)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = getString(R.string.app_name)
+        val channelName = getString(R.string.app_name)
+        val channelImportance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(channelId, channelName, channelImportance)
+        notificationManager.createNotificationChannel(channel)
+        notificationManager.notify(alarmId, builder.build())
     }
 }
