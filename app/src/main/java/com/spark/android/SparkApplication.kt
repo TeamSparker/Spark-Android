8ee6a0ac719344a424629f930c6aeb58b26cbcf0
changhwan <chang5hustle@gmail.com>
2022-01-16 11:41:14 +0900

    #4 [refactor] 컨텍스트 관련 수정

--- app/src/main/java/com/spark/android/SparkApplication.kt
+++ app/src/main/java/com/spark/android/SparkApplication.kt
@@ -10,16 +10,5 @@ class SparkApplication : Application() {
     override fun onCreate() {
         super.onCreate()
     }
-
-    // 이거 컨텍스트 사용하려고 넣어놨는데 확인후 사용용
-   init{
-        instance = this
-    }
-
-    companion object {
-        lateinit var instance: SparkApplication
-        fun ApplicationContext() : Context {
-            return instance.applicationContext
-        }
-    }
+    
 }
