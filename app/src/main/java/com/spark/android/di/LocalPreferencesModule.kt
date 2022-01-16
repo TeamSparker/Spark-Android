1a6b4951f0b992eb3514d0ed16a88e0118ed8bf0
Yunjoo Son <thsduswn4141@naver.com>
2022-01-16 17:30:30 +0900

    #54 [feat] Hilt 의존성 주입 모듈 생성

---
+++ app/src/main/java/com/spark/android/di/LocalPreferencesModule.kt
@@ -0,0 +1,27 @@
+package com.spark.android.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalPreferencesModule {
+    @Provides
+    @Singleton
+    fun providesLocalPreferences(@ApplicationContext context: Context): SharedPreferences =
+        EncryptedSharedPreferences.create(
+            context,
+            context.packageName,
+            MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build(),
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+}
