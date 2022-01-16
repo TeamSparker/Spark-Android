9f2f3fd80d692abe8f5ec7d54fbe40785b77ebc5
Grandelwald <artjr97@naver.com>
2022-01-16 16:17:39 +0900

    #56 [chore] timer 시작 뷰 1차 완성

---
+++ app/src/main/java/com/spark/android/ui/timer/TimerStartActivity.kt
@@ -0,0 +1,12 @@
+package com.spark.android.ui.timer
+
+import android.os.Bundle
+import com.spark.android.R
+import com.spark.android.databinding.ActivityTimerStartBinding
+import com.spark.android.ui.base.BaseActivity
+
+class TimerStartActivity : BaseActivity<ActivityTimerStartBinding>(R.layout.activity_timer_start) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
