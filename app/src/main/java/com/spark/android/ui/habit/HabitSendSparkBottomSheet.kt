4f9fbe19b9e8d030e3306bee9b1135d87b4b19bd
Jaehun-Jo <chojh922@gmail.com>
2022-01-17 05:23:33 +0900

    [feat] 습관방 스파크 보내기 toast 메시지 연결

--- app/src/main/java/com/spark/android/ui/habit/HabitSendSparkBottomSheet.kt
+++ app/src/main/java/com/spark/android/ui/habit/HabitSendSparkBottomSheet.kt
@@ -7,8 +7,8 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.spark.android.R
-import com.spark.android.databinding.BottomSheetHabitMoreBinding
 import com.spark.android.databinding.BottomSheetHabitSendSparkBinding
+import com.spark.android.util.SendSparkToast
 
 class HabitSendSparkBottomSheet : BottomSheetDialogFragment() {
     private var _binding: BottomSheetHabitSendSparkBinding? = null
@@ -30,10 +30,42 @@ class HabitSendSparkBottomSheet : BottomSheetDialogFragment() {
         val behavior = BottomSheetBehavior.from<View>(bottomSheet!!)
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
 
+        initSendFirstBtnClickListener(view)
+        initSendSecondBtnClickListener(view)
+        initSendThirdBtnClickListener(view)
+        initSendFourthBtnClickListener(view)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initSendFirstBtnClickListener(view: View) {
+        binding.btnHabitSendSparkMessageFirst.setOnClickListener {
+            SendSparkToast.showToast(view.context)
+            dismiss()
+        }
+    }
+
+    private fun initSendSecondBtnClickListener(view: View) {
+        binding.btnHabitSendSparkMessageSecond.setOnClickListener {
+            SendSparkToast.showToast(view.context)
+            dismiss()
+        }
+    }
+
+    private fun initSendThirdBtnClickListener(view: View) {
+        binding.btnHabitSendSparkMessageThird.setOnClickListener {
+            SendSparkToast.showToast(view.context)
+            dismiss()
+        }
+    }
+
+    private fun initSendFourthBtnClickListener(view: View) {
+        binding.btnHabitSendSparkMessageFourth.setOnClickListener {
+            SendSparkToast.showToast(view.context)
+            dismiss()
+        }
     }
 }
