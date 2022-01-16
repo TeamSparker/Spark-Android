9586886773d61e2e2011b3be15080795c381794d
Grandelwald <artjr97@naver.com>
2022-01-17 06:52:08 +0900

    #56 [feat] 다빈의 도움으로 타이머 기능 구현

--- app/src/main/java/com/spark/android/ui/timer/TimerStartActivity.kt
+++ app/src/main/java/com/spark/android/ui/timer/TimerStartActivity.kt
@@ -1,40 +1,115 @@
 package com.spark.android.ui.timer
 
 import android.os.Bundle
-import com.spark.android.databinding.ActivityTimerStartBinding
 import com.spark.android.ui.base.BaseActivity
 import android.widget.Chronometer
 import android.widget.Chronometer.OnChronometerTickListener
 import android.os.SystemClock
 import android.view.View
+import android.widget.Toast
+import com.spark.android.R
+import androidx.activity.viewModels
+import com.spark.android.databinding.ActivityTimerStartBinding
+import com.spark.android.ui.timer.viewmodel.TimerStartViewModel
+import com.spark.android.ui.timer.viewmodel.TimerStartViewModel.Companion.TIMER_PAUSE
+import com.spark.android.ui.timer.viewmodel.TimerStartViewModel.Companion.TIMER_RESET
+import com.spark.android.ui.timer.viewmodel.TimerStartViewModel.Companion.TIMER_RUN
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class TimerStartActivity :
-    BaseActivity<ActivityTimerStartBinding>(com.spark.android.R.layout.activity_timer_start) {
+class TimerStartActivity : BaseActivity<ActivityTimerStartBinding>(R.layout.activity_timer_start) {
+    private val timerStartViewModel by viewModels<TimerStartViewModel>()
+    var pauseTime = 0L
+    //  private lateinit var timer: Chronometer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.timerStartViewModel = timerStartViewModel
 
-        initTimerFormatChange()
+        initFormatChange()
+        initClickEvent()
 
-
-
+        //initTimerStateObserver()
     }
 
-    private fun initTimerFormatChange() {
-        val timeElapsed =
-            findViewById<View>(com.spark.android.R.id.chronometer_timer_stopwatch) as Chronometer
-        timeElapsed.onChronometerTickListener =
-            OnChronometerTickListener { cArg ->
-                val time: Long = SystemClock.elapsedRealtime() - cArg.base
+//    private fun initTimerStateObserver() {
+//        timerStartViewModel.timerState.observe(this) {
+//            when (it) {
+//                TIMER_RESET -> {
+//                    binding.chronometerTimer.base = SystemClock.elapsedRealtime()
+//                }
+//                TIMER_RUN -> {
+//                    binding.chronometerTimer.base = SystemClock.elapsedRealtime()
+//                    binding.chronometerTimer.start()
+//                }
+//                TIMER_PAUSE -> {
+//                    binding.chronometerTimer.stop()
+//                }
+//            }
+//        }
+//    }
+
+    private fun initClickEvent() {
+        binding.btnTimerStartBottom.setOnClickListener {
+            timerStartViewModel.initTimerRun()
+            binding.btnTimerStartBottom.visibility = View.INVISIBLE
+            binding.btnTimerPause.visibility = View.VISIBLE
+            binding.btnTimerStop.visibility = View.VISIBLE
+
+            binding.chronometerTimer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometerTimer.start()
+        }
+
+        binding.btnTimerStop.setOnClickListener {
+            timerStartViewModel.initTimerReset()
+            binding.btnTimerStop.visibility = View.INVISIBLE
+            binding.btnTimerPause.visibility = View.INVISIBLE
+            binding.btnTimerPlay.visibility = View.INVISIBLE
+            binding.btnTimerStartBottom.visibility = View.VISIBLE
+
+            pauseTime = 0L
+            binding.chronometerTimer.base = SystemClock.elapsedRealtime()
+            initFormatChange()
+            binding.chronometerTimer.stop()
+
+        }
+
+        binding.btnTimerPlay.setOnClickListener {
+            timerStartViewModel.initTimerRun()
+            binding.btnTimerPlay.visibility = View.INVISIBLE
+            binding.btnTimerPause.visibility = View.VISIBLE
+
+            binding.chronometerTimer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometerTimer.start()
+        }
+
+        binding.btnTimerPause.setOnClickListener {
+            timerStartViewModel.initTimerPause()
+            binding.btnTimerPause.visibility = View.INVISIBLE
+            binding.btnTimerPlay.visibility = View.VISIBLE
+
+            pauseTime = binding.chronometerTimer.base - SystemClock.elapsedRealtime()
+            binding.chronometerTimer.stop()
+
+        }
+    }
+
+    private fun initFormatChange() {
+        val chrono = binding.chronometerTimer
+        chrono.base = SystemClock.elapsedRealtime()
+        chrono.text = "00:00:00"
+
+        chrono.onChronometerTickListener =
+            OnChronometerTickListener { chronometer ->
+                val time = SystemClock.elapsedRealtime() - chronometer.base
                 val h = (time / 3600000).toInt()
                 val m = (time - h * 3600000).toInt() / 60000
                 val s = (time - h * 3600000 - m * 60000).toInt() / 1000
-                val hh = if (h < 10) "0$h" else h.toString() + ""
-                val mm = if (m < 10) "0$m" else m.toString() + ""
-                val ss = if (s < 10) "0$s" else s.toString() + ""
-                cArg.text = "$hh:$mm:$ss"
+                val t =
+                    (if (h < 10) "0$h" else h).toString() + ":" + (if (m < 10) "0$m" else m) + ":" + if (s < 10) "0$s" else s
+                chronometer.text = t
             }
-        timeElapsed.base = SystemClock.elapsedRealtime()
-        timeElapsed.start()
     }
 }
--- app/src/main/java/com/spark/android/ui/timer/viewmodel/TimerStartViewModel.kt
+++ app/src/main/java/com/spark/android/ui/timer/viewmodel/TimerStartViewModel.kt
@@ -4,15 +4,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class TimerStartViewModel : ViewModel(){
+class TimerStartViewModel : ViewModel() {
 
-//    private val _sparkMode = MutableLiveData(0)
-//    val sparkMode: LiveData<Int> = _sparkMode
+    private val _timerState = MutableLiveData(TIMER_RESET)
+    val timerState: LiveData<Int> get() = _timerState
 
-    private val _timerBottomBtnClick = MutableLiveData(false)
-    val timerBottomBtnClick:LiveData<Boolean> = _timerBottomBtnClick
+    fun initTimerReset() {
+        _timerState.value = TIMER_RESET
+    }
+
+    fun initTimerRun() {
+        _timerState.value = TIMER_RUN
+    }
+
+    fun initTimerPause() {
+        _timerState.value = TIMER_PAUSE
+    }
 
 
-
-
+    companion object {
+        const val TIMER_RESET = 0
+        const val TIMER_RUN = 1
+        const val TIMER_PAUSE = 2
+    }
 }
