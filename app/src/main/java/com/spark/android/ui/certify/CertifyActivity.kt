0369925e63b81c2e17fb1a1616c300f48a21095f
Jaehun-Jo <chojh922@gmail.com>
2022-01-17 19:07:40 +0900

    [feat] 사진 인증하기 바텀 시트 다이얼로그 추가

--- app/src/main/java/com/spark/android/ui/certify/CertifyActivity.kt
+++ app/src/main/java/com/spark/android/ui/certify/CertifyActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import com.spark.android.R
 import com.spark.android.databinding.ActivityCertifyBinding
 import com.spark.android.ui.base.BaseActivity
+import com.spark.android.ui.habit.HabitMoreBottomSheet
 
 class CertifyActivity : BaseActivity<ActivityCertifyBinding>(R.layout.activity_certify) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initCertifyPhotoBtnClickListener()
+    }
+
+    private fun initCertifyPhotoBtnClickListener() {
+        binding.btnCertifyPhoto.setOnClickListener {
+            CertifyBottomSheet().show(supportFragmentManager, this.javaClass.name)
+        }
     }
 }
