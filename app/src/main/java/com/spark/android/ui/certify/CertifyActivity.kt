0b38da35ee4bc9a8465dc7794e607d5bbdc8dbc6
Jaehun-Jo <chojh922@gmail.com>
2022-01-17 18:54:10 +0900

    [feat] 사진 인증 시작 뷰 추가

---
+++ app/src/main/java/com/spark/android/ui/certify/CertifyActivity.kt
@@ -0,0 +1,13 @@
+package com.spark.android.ui.certify
+
+import android.os.Bundle
+import com.spark.android.R
+import com.spark.android.databinding.ActivityCertifyBinding
+import com.spark.android.ui.base.BaseActivity
+
+class CertifyActivity : BaseActivity<ActivityCertifyBinding>(R.layout.activity_certify) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/spark/android/ui/certify/viewmodel/CertifyViewModel.kt
@@ -0,0 +1,8 @@
+package com.spark.android.ui.certify.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CertifyViewModel  : ViewModel() {
+    private val test = MutableLiveData("")
+}
