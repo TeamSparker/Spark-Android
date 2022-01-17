f443e7932f97fef25c90de01a7e3f3ddf68a3722
Grandelwald <artjr97@naver.com>
2022-01-18 03:15:11 +0900

    #86 [타이머] x버튼, 뒤로가기 버튼, 다이얼로그 붙임

---
+++ app/src/main/java/com/spark/android/data/remote/service/StorageService.kt
@@ -0,0 +1,14 @@
+package com.spark.android.data.remote.service
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+//
+//interface StorageService {
+//
+//    @GET("myroom")
+//    fun getStorageList(
+//        @Query("type") type:String,
+//        @Query("lastid") lastid:Int,
+//        @Query("size") size:Int
+//    ):
+//}
