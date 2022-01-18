17f584c1cbd59034d1dd7c7840c82ae474c8f11f
Jaehun-Jo <chojh922@gmail.com>
2022-01-18 15:31:53 +0900

    [modify] 쉴래요 버튼 클릭 리스너 추가

--- app/src/main/java/com/spark/android/ui/habit/HabitTodayBottomSheet.kt
+++ app/src/main/java/com/spark/android/ui/habit/HabitTodayBottomSheet.kt
@@ -35,6 +35,7 @@ class HabitTodayBottomSheet(private val remainCount: Int) : BottomSheetDialogFra
 
         initCertifyBtnClickListener()
         initConsiderBtnClickListener()
+        initRestBtnClickListener()
     }
 
     private fun initCertifyBtnClickListener() {
