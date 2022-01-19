22aeabb813cf20dd60009fcefff1377e5b671899
Yunjoo Son <thsduswn4141@naver.com>
2022-01-20 06:44:17 +0900

    #145 [feat] 회원여부 판별 서버 연결 위한 구조 설정

--- app/src/main/java/com/spark/android/data/remote/datasource/AuthDataSource.kt
+++ app/src/main/java/com/spark/android/data/remote/datasource/AuthDataSource.kt
@@ -1,6 +1,7 @@
 package com.spark.android.data.remote.datasource
 
 import com.spark.android.data.remote.entity.response.BaseResponse
+import com.spark.android.data.remote.entity.response.DoorbellResponse
 import com.spark.android.data.remote.entity.response.SignUpResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -10,4 +11,6 @@ interface AuthDataSource {
         map: Map<String, RequestBody>,
         profileImg: MultipartBody.Part?
     ): BaseResponse<SignUpResponse>
+
+    suspend fun getAccessToken(socialId: String, fcmToken: String): BaseResponse<DoorbellResponse>
 }
--- app/src/main/java/com/spark/android/data/remote/datasource/AuthDataSourceImpl.kt
+++ app/src/main/java/com/spark/android/data/remote/datasource/AuthDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.spark.android.data.remote.datasource
 
 import com.spark.android.data.remote.entity.response.BaseResponse
+import com.spark.android.data.remote.entity.response.DoorbellResponse
 import com.spark.android.data.remote.entity.response.SignUpResponse
 import com.spark.android.data.remote.service.AuthService
 import okhttp3.MultipartBody
@@ -20,4 +21,10 @@ class AuthDataSourceImpl @Inject constructor(
             authService.postSignUpWithImg(map, profileImg)
         }
     }
+
+    override suspend fun getAccessToken(
+        socialId: String,
+        fcmToken: String
+    ): BaseResponse<DoorbellResponse> =
+        authService.getAccessToken(socialId, fcmToken)
 }
