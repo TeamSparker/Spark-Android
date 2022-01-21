d67f83dfdc824a7efa718257f09fb42f111c18fe
Yunjoo Son <thsduswn4141@naver.com>
2022-01-22 06:12:42 +0900

    #171 [feat] 습관방 로딩뷰 추가

--- app/src/main/java/com/spark/android/ui/habit/viewmodel/HabitViewModel.kt
+++ app/src/main/java/com/spark/android/ui/habit/viewmodel/HabitViewModel.kt
@@ -20,6 +20,9 @@ class HabitViewModel : ViewModel() {
     private val setStatusService: SetStatusService = RetrofitBuilder.setStatusService
     private val sendSparkService: SendSparkService = RetrofitBuilder.sendSparkService
 
+    private val _isLoading = MutableLiveData(false)
+    val isLoading: LiveData<Boolean> = _isLoading
+
     private val _habitInfo = MutableLiveData<HabitResponse>()
     val habitInfo: LiveData<HabitResponse> = _habitInfo
 
@@ -29,34 +32,47 @@ class HabitViewModel : ViewModel() {
     private val _sendSuccess = MutableLiveData<Boolean>()
     val sendSuccess: LiveData<Boolean> = _sendSuccess
 
+    fun initIsLoading(isLoading: Boolean) {
+        _isLoading.value = isLoading
+    }
+
     fun initSendSuccess(success: Boolean) {
         _sendSuccess.value = success
     }
 
     fun getHabitRoomInfo(roomId: Int) {
+        initIsLoading(true)
         viewModelScope.launch {
-            val response = habitService.getHabitRoom(roomId)
-            val data = response.data ?: throw NullPointerException("습관방 통신 에러")
-            _habitInfo.postValue(data)
+            kotlin.runCatching {
+                habitService.getHabitRoom(roomId)
+            }.onSuccess { response ->
+                val data = response.data ?: throw NullPointerException("습관방 통신 에러")
+                _habitInfo.postValue(data)
 
-            _habitRecordList.postValue(mutableListOf<HabitRecord>().apply {
-                add(data.myRecord)
-                addAll(data.otherRecords.map {
-                    HabitRecord(nickname = it.nickname,
-                        profileImg = it.profileImg,
-                        recordId = it.recordId,
-                        status = it.status,
-                        userId = it.userId)
+                _habitRecordList.postValue(mutableListOf<HabitRecord>().apply {
+                    add(data.myRecord)
+                    addAll(data.otherRecords.map {
+                        HabitRecord(
+                            nickname = it.nickname,
+                            profileImg = it.profileImg,
+                            recordId = it.recordId,
+                            status = it.status,
+                            userId = it.userId
+                        )
+                    })
                 })
-            })
+                initIsLoading(false)
+            }.onFailure { }
         }
     }
 
     fun postStatus(statusType: String) {
         viewModelScope.launch {
             habitInfo.value?.let {
-                setStatusService.setStatus(it.roomId,
-                    SetStatusRequest(statusType))
+                setStatusService.setStatus(
+                    it.roomId,
+                    SetStatusRequest(statusType)
+                )
             }
         }
     }
@@ -65,8 +81,10 @@ class HabitViewModel : ViewModel() {
         viewModelScope.launch {
             habitInfo.value?.let {
                 kotlin.runCatching {
-                    sendSparkService.sendSpark(it.roomId,
-                        SendSparkRequest(content, recordId))
+                    sendSparkService.sendSpark(
+                        it.roomId,
+                        SendSparkRequest(content, recordId)
+                    )
                 }.onSuccess { _sendSuccess.value = true }
 
             }
