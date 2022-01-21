6aa871e4b7fdc3f7add1e0d6f81961ee89397385
Yunjoo Son <thsduswn4141@naver.com>
2022-01-22 08:14:12 +0900

    #171 [feat] 보관함 인디케이터 애니메이션 적용

--- app/src/main/java/com/spark/android/ui/storage/StorageFragment.kt
+++ app/src/main/java/com/spark/android/ui/storage/StorageFragment.kt
@@ -1,5 +1,6 @@
 package com.spark.android.ui.storage
 
+import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
@@ -17,6 +18,9 @@ import java.lang.IllegalStateException
 class StorageFragment : BaseFragment<FragmentStorageBinding>(R.layout.fragment_storage) {
     private lateinit var viewPagerOutAdapter: StorageViewPagerOutAdapter
     private val storageViewModel by activityViewModels<StorageViewModel>()
+    private val progressingIndicatorAnim by lazy { getIndicatorAnimator(binding.viewStorageProgressingIndicator) }
+    private val incompleteIndicatorAnim by lazy { getIndicatorAnimator(binding.viewStorageIncompleteIndicator) }
+    private val completeIndicatorAnim by lazy { getIndicatorAnimator(binding.viewStorageCompleteIndicator) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,19 +52,30 @@ class StorageFragment : BaseFragment<FragmentStorageBinding>(R.layout.fragment_s
             when (mode) {
                 PROGRESSING -> {
                     binding.vpStorageOut.currentItem = 0
+                    progressingIndicatorAnim.start()
                     storageViewModel.initStorageNetwork(PROGRESSING, -1, 30)
                 }
                 COMPLETE -> {
                     binding.vpStorageOut.currentItem = 1
+                    completeIndicatorAnim.start()
                     storageViewModel.initStorageNetwork(COMPLETE, -1, 30)
                 }
                 INCOMPLETE -> {
                     binding.vpStorageOut.currentItem = 2
+                    incompleteIndicatorAnim.start()
                     storageViewModel.initStorageNetwork(INCOMPLETE, -1, 30)
                 }
                 else -> throw IllegalStateException()
             }
         }
     }
+
+    private fun getIndicatorAnimator(view: View) = AnimatorInflater
+        .loadAnimator(
+            context,
+            R.animator.animator_storage_indicator
+        ).apply {
+            setTarget(view)
+        }
 
 }
