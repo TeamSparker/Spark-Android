d5d2b49f4a51e449b9d9f120a2f87821399d130c
Yunjoo Son <thsduswn4141@naver.com>
2022-02-17 02:21:59 +0900

    #240 [fix] 총 스파크 수 필드명 수정

--- app/src/main/java/com/spark/android/data/remote/entity/response/StorageResponse.kt
+++ app/src/main/java/com/spark/android/data/remote/entity/response/StorageResponse.kt
@@ -1,8 +1,5 @@
 package com.spark.android.data.remote.entity.response
 
-
-import com.google.gson.annotations.SerializedName
-
 data class StorageResponse(
     val nickname: String,
     val totalRoomNum: Int,
@@ -17,10 +14,10 @@ data class StorageRoom(
     val roomName: String,
     val leftDay: Int,
     val thumbnail: String,
-    val totalRecievedSpark: Int,
+    val totalReceivedSpark: Int,
     val startDate: String,
     val endDate: String,
-    val failDay: Int ,
+    val failDay: Int,
     val comment: Any
 )
 
