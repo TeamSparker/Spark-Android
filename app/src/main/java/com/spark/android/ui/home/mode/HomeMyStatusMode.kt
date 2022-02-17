7d45727814ee3784edbed034f3cdf37ff06739b1
changhwan <chang5hustle@gmail.com>
2022-02-18 03:49:10 +0900

    #242 [feat] 홈에 rest 딱지 붙은거 처리 데이터바인딩 (문자열 ``로 바꿈)

--- app/src/main/java/com/spark/android/ui/home/mode/HomeMyStatusMode.kt
+++
@@ -1,9 +0,0 @@
-package com.spark.android.ui.home.mode
-
-class HomeMyStatusMode {
-    companion object{
-        const val REST = "REST"
-        const val DONE = "DONE"
-        const val NONE = "NONE"
-    }
-}
