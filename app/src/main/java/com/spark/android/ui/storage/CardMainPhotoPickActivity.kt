782e733cb98275227ef315484d6c1d2e337d8ad1
Grandelwald <artjr97@naver.com>
2022-02-21 17:03:58 +0900

    #261 [layout] CardMainPhotoPickActivity.kt 생성

---
+++ app/src/main/java/com/spark/android/ui/storage/CardMainPhotoPickActivity.kt
@@ -0,0 +1,12 @@
+package com.spark.android.ui.storage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spark.android.R
+
+class CardMainPhotoPickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_main_photo_pick)
+    }
+}
