cf7b839bb8da4bc3c88a3aeeae835018cdea6661
Yunjoo Son <thsduswn4141@naver.com>
2022-02-26 22:08:30 +0900

    #257 [feat] 푸쉬 알림 카테고리별 그룹핑 작업

--- app/src/main/java/com/spark/android/SparkMessagingService.kt
+++ app/src/main/java/com/spark/android/SparkMessagingService.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
@@ -16,8 +17,14 @@ import com.google.firebase.messaging.RemoteMessage
 import com.spark.android.ui.intro.IntroActivity
 import com.spark.android.util.ImageUrlTransformer
 import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
 
 class SparkMessagingService : FirebaseMessagingService() {
+    data class NotificationCategory(
+        val summaryId: Int,
+        val groupName: String
+    )
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
     }
@@ -35,6 +42,7 @@ class SparkMessagingService : FirebaseMessagingService() {
 
     private fun showNotificationWithoutImage(remoteMessage: RemoteMessage) {
         val alarmId = remoteMessage.sentTime.toInt()
+        val category = remoteMessage.data["category"].toString()
         val intent = Intent(this, IntroActivity::class.java)
         val pendingIntent =
             PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
@@ -46,6 +54,8 @@ class SparkMessagingService : FirebaseMessagingService() {
                 .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setAutoCancel(true)
+                .setGroup(category)
+
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val channelId = getString(R.string.app_name)
@@ -54,10 +64,12 @@ class SparkMessagingService : FirebaseMessagingService() {
         val channel = NotificationChannel(channelId, channelName, channelImportance)
         notificationManager.createNotificationChannel(channel)
         notificationManager.notify(alarmId, builder.build())
+        notificationManager.notify(getSummaryId(category), getSummary(category).build())
     }
 
     private fun showNotificationWithImage(remoteMessage: RemoteMessage, bitmap: Bitmap) {
         val alarmId = remoteMessage.sentTime.toInt()
+        val category = remoteMessage.data["category"].toString()
         val intent = Intent(this, IntroActivity::class.java)
         val pendingIntent =
             PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
@@ -69,6 +81,7 @@ class SparkMessagingService : FirebaseMessagingService() {
             .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_MAX)
             .setAutoCancel(true)
+            .setGroup(category)
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -78,6 +91,7 @@ class SparkMessagingService : FirebaseMessagingService() {
         val channel = NotificationChannel(channelId, channelName, channelImportance)
         notificationManager.createNotificationChannel(channel)
         notificationManager.notify(alarmId, builder.build())
+        notificationManager.notify(getSummaryId(category), getSummary(category).build())
     }
 
     private fun transformImageUrlToBitmap(remoteMessage: RemoteMessage) {
@@ -95,5 +109,29 @@ class SparkMessagingService : FirebaseMessagingService() {
 
                 override fun onLoadCleared(placeholder: Drawable?) {}
             })
+    }
+
+    private fun getSummary(category: String) =
+        NotificationCompat.Builder(this, getString(R.string.app_name))
+            .setSmallIcon(R.mipmap.ic_app_logo)
+            .setContentTitle(getString(R.string.app_name))
+            .setGroup(category)
+            .setGroupSummary(true)
+
+    private fun getSummaryId(category: String) = when (category) {
+        CATEGORY_CERTIFICATION.groupName -> CATEGORY_CERTIFICATION.summaryId
+        CATEGORY_SPARK.groupName -> CATEGORY_SPARK.summaryId
+        CATEGORY_REMIND.groupName -> CATEGORY_REMIND.summaryId
+        CATEGORY_ROOM_START.groupName -> CATEGORY_ROOM_START.summaryId
+        CATEGORY_CONSIDER.groupName -> CATEGORY_CONSIDER.summaryId
+        else -> throw IllegalArgumentException("FCM category 필드 오류")
+    }
+
+    companion object {
+        private val CATEGORY_CERTIFICATION = NotificationCategory(0, "certification")
+        private val CATEGORY_SPARK = NotificationCategory(1, "spark")
+        private val CATEGORY_REMIND = NotificationCategory(2, "remind")
+        private val CATEGORY_ROOM_START = NotificationCategory(3, "roomStart")
+        private val CATEGORY_CONSIDER = NotificationCategory(4, "consider")
     }
 }
