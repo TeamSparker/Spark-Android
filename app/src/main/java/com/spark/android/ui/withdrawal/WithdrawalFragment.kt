88579e783fb617bb508d73bd6345b510cea5114e
Yunjoo Son <thsduswn4141@naver.com>
2022-03-08 18:56:31 +0900

    #277 [chore] 회원 탈퇴 Fragment 추가

--- app/src/main/java/com/spark/android/ui/withdrawal/WithdrawalFragment.kt
+++ app/src/main/java/com/spark/android/ui/withdrawal/WithdrawalFragment.kt
@@ -1,17 +1,18 @@
-package com.spark.android
+package com.spark.android.ui.withdrawal
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.spark.android.R
 
-class AlarmSettingFragment : Fragment() {
+class WithdrawalFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm_setting, container, false)
+        return inflater.inflate(R.layout.fragment_withdrawal, container, false)
     }
 }
