17c567a05933a4092195aae1bca9ce9477531343
Yunjoo Son <thsduswn4141@naver.com>
2022-03-12 01:09:47 +0900

    #314 [feat] MyPageMain에서 Profile로 화면 이동 구현

--- app/src/main/java/com/spark/android/ui/mypage/MyPageActivity.kt
+++ app/src/main/java/com/spark/android/ui/mypage/MyPageActivity.kt
@@ -3,7 +3,9 @@ package com.spark.android.ui.mypage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.spark.android.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/spark/android/ui/mypage/MyPageMainFragment.kt
+++ app/src/main/java/com/spark/android/ui/mypage/MyPageMainFragment.kt
@@ -5,14 +5,23 @@ import android.view.View
 import com.spark.android.R
 import com.spark.android.databinding.FragmentMyPageMainBinding
 import com.spark.android.ui.base.BaseFragment
+import com.spark.android.util.navigate
 
 class MyPageMainFragment : BaseFragment<FragmentMyPageMainBinding>(R.layout.fragment_my_page_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initBackBtnClickListener()
+        initModifyProfileBtnClickListener()
     }
 
     private fun initBackBtnClickListener() {
         binding.btnMyPageMainBack.setOnClickListener { requireActivity().finish() }
     }
+
+    private fun initModifyProfileBtnClickListener() {
+        binding.btnMyPageMainModifyProfile.setOnClickListener {
+            navigate(R.id.action_myPageMainFragment_to_profileFragmentFromMyPage)
+        }
+    }
+
 }
