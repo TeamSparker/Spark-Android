4a3730af8f0d988a4363fe20cd4348a2a5f8d9e2
Yunjoo Son <thsduswn4141@naver.com>
2022-03-13 18:07:51 +0900

    #327 [feat] 마이페이지 진입 시 statusbar 설정

--- app/src/main/java/com/spark/android/ui/mypage/MyPageActivity.kt
+++ app/src/main/java/com/spark/android/ui/mypage/MyPageActivity.kt
@@ -3,6 +3,8 @@ package com.spark.android.ui.mypage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.spark.android.R
+import com.spark.android.util.initStatusBarColor
+import com.spark.android.util.initStatusBarTextColorToWhite
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +12,11 @@ class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+        initStatusBarStyle()
+    }
+
+    private fun initStatusBarStyle() {
+        initStatusBarColor(R.color.spark_white)
+        initStatusBarTextColorToWhite()
     }
 }
