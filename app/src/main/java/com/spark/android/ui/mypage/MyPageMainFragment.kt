9c9f74f79f84176c077280a44b7d7b3bba0434ee
Yunjoo Son <thsduswn4141@naver.com>
2022-03-13 17:15:07 +0900

    #326 [feat] 마이페이지 프로필 조회 후 데이터 바인딩

--- app/src/main/java/com/spark/android/ui/mypage/MyPageMainFragment.kt
+++ app/src/main/java/com/spark/android/ui/mypage/MyPageMainFragment.kt
@@ -2,14 +2,21 @@ package com.spark.android.ui.mypage
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.spark.android.R
 import com.spark.android.databinding.FragmentMyPageMainBinding
 import com.spark.android.ui.base.BaseFragment
 import com.spark.android.util.navigate
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyPageMainFragment : BaseFragment<FragmentMyPageMainBinding>(R.layout.fragment_my_page_main) {
+    private val myPageViewModel by viewModels<MyPageViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.myPageViewModel = myPageViewModel
+        myPageViewModel.getProfile()
         initBackBtnClickListener()
         initModifyProfileBtnClickListener()
         initAlarmSettingBtnClickListener()
@@ -37,5 +44,4 @@ class MyPageMainFragment : BaseFragment<FragmentMyPageMainBinding>(R.layout.frag
             navigate(R.id.action_myPageMainFragment_to_withdrawalFragment)
         }
     }
-
 }
--- app/src/main/java/com/spark/android/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/spark/android/ui/mypage/MyPageViewModel.kt
@@ -1,12 +1,31 @@
 package com.spark.android.ui.mypage
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.spark.android.data.remote.entity.response.ProfileResponse
 import com.spark.android.data.remote.repository.ProfileRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
     private val profileRepository: ProfileRepository
-) :ViewModel() {
+) : ViewModel() {private val _profileData = MutableLiveData<ProfileResponse>()
+    val profileData: LiveData<ProfileResponse> = _profileData
+
+    fun getProfile() {
+        viewModelScope.launch {
+            profileRepository.getProfile()
+                .onSuccess { response ->
+                    _profileData.postValue(requireNotNull(response.data))
+                }
+                .onFailure {
+                    Log.d("MyPage_GetProfile", it.message.toString())
+                }
+        }
+    }
 }
