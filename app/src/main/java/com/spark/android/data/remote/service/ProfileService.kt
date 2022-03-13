95d85e13b8d5ef7d312913c3290ebd3857df6568
Yunjoo Son <thsduswn4141@naver.com>
2022-03-13 18:22:51 +0900

    #327 [feat] 프로필 수정 관련 Api 정의

--- app/src/main/java/com/spark/android/data/remote/service/ProfileService.kt
+++ app/src/main/java/com/spark/android/data/remote/service/ProfileService.kt
@@ -1,10 +1,27 @@
 package com.spark.android.data.remote.service
 
 import com.spark.android.data.remote.entity.response.BaseResponse
+import com.spark.android.data.remote.entity.response.NoDataResponse
 import com.spark.android.data.remote.entity.response.ProfileResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Part
+import retrofit2.http.PartMap
 
 interface ProfileService {
     @GET("user/profile")
     suspend fun getProfile(): BaseResponse<ProfileResponse>
+
+    @PATCH("user/profile")
+    suspend fun patchProfileWithImg(
+        @PartMap map: Map<String, @JvmSuppressWildcards RequestBody>,
+        @Part profileImg: MultipartBody.Part
+    ): NoDataResponse
+
+    @PATCH("user/profile")
+    suspend fun patchProfile(
+        @PartMap map: Map<String, @JvmSuppressWildcards RequestBody>
+    ): NoDataResponse
 }
