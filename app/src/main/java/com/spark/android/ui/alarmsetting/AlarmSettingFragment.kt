4cd8608f31b89d4aeb70e07f1c63a758f6bec136
Yunjoo Son <thsduswn4141@naver.com>
2022-03-13 23:05:51 +0900

    #328 [feat] 알림 설정 변경 서버 연결 후 데이터바인딩

--- app/src/main/java/com/spark/android/ui/alarmsetting/AlarmSettingFragment.kt
+++ app/src/main/java/com/spark/android/ui/alarmsetting/AlarmSettingFragment.kt
@@ -5,6 +5,11 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.spark.android.R
 import com.spark.android.databinding.FragmentAlarmSettingBinding
+import com.spark.android.ui.alarmsetting.AlarmSettingViewModel.Companion.ALARM_CERTIFICATION
+import com.spark.android.ui.alarmsetting.AlarmSettingViewModel.Companion.ALARM_CONSIDER
+import com.spark.android.ui.alarmsetting.AlarmSettingViewModel.Companion.ALARM_REMIND
+import com.spark.android.ui.alarmsetting.AlarmSettingViewModel.Companion.ALARM_ROOM_START
+import com.spark.android.ui.alarmsetting.AlarmSettingViewModel.Companion.ALARM_SPARK
 import com.spark.android.ui.base.BaseFragment
 import com.spark.android.util.popBackStack
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,9 +24,44 @@ class AlarmSettingFragment :
         binding.alarmSettingViewModel = alarmSettingViewModel
         alarmSettingViewModel.getAlarmSetting()
         initBackBtnClickListener()
+        initCertificationSwitchClickListener()
+        initConsiderSwitchClickListener()
+        initReminderSwitchClickListener()
+        initRoomStartSwitchClickListener()
+        initSparkSwitchClickListener()
     }
 
     private fun initBackBtnClickListener() {
         binding.btnAlarmSettingBack.setOnClickListener { popBackStack() }
+    }
+
+    private fun initCertificationSwitchClickListener() {
+        binding.switchAlarmSettingCertification.setOnClickListener {
+            alarmSettingViewModel.patchAlarmSetting(ALARM_CERTIFICATION)
+        }
+    }
+
+    private fun initConsiderSwitchClickListener() {
+        binding.switchAlarmSettingConsider.setOnClickListener {
+            alarmSettingViewModel.patchAlarmSetting(ALARM_CONSIDER)
+        }
+    }
+
+    private fun initReminderSwitchClickListener() {
+        binding.switchAlarmSettingNotDone.setOnClickListener {
+            alarmSettingViewModel.patchAlarmSetting(ALARM_REMIND)
+        }
+    }
+
+    private fun initRoomStartSwitchClickListener() {
+        binding.switchAlarmSettingRoomStart.setOnClickListener {
+            alarmSettingViewModel.patchAlarmSetting(ALARM_ROOM_START)
+        }
+    }
+
+    private fun initSparkSwitchClickListener() {
+        binding.switchAlarmSettingSendSpark.setOnClickListener {
+            alarmSettingViewModel.patchAlarmSetting(ALARM_SPARK)
+        }
     }
 }
--- app/src/main/java/com/spark/android/ui/alarmsetting/AlarmSettingViewModel.kt
+++ app/src/main/java/com/spark/android/ui/alarmsetting/AlarmSettingViewModel.kt
@@ -1,6 +1,7 @@
 package com.spark.android.ui.alarmsetting
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,29 +9,56 @@ import com.spark.android.data.remote.repository.AlarmSettingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.properties.Delegates
 
 @HiltViewModel
 class AlarmSettingViewModel @Inject constructor(
     private val alarmSettingRepository: AlarmSettingRepository
 ) : ViewModel() {
-    val isOnCertification = MutableLiveData<Boolean>()
-    val isOnConsider = MutableLiveData<Boolean>()
-    val isOnRemind = MutableLiveData<Boolean>()
-    val isOnRoomStart = MutableLiveData<Boolean>()
-    val isOnSpark = MutableLiveData<Boolean>()
+    private val _isOnCertification = MutableLiveData<Boolean>()
+    val isOnCertification: LiveData<Boolean> = _isOnCertification
+
+    private val _isOnConsider = MutableLiveData<Boolean>()
+    val isOnConsider: LiveData<Boolean> = _isOnConsider
+
+    private val _isOnRemind = MutableLiveData<Boolean>()
+    val isOnRemind: LiveData<Boolean> = _isOnRemind
+
+    private val _isOnRoomStart = MutableLiveData<Boolean>()
+    val isOnRoomStart: LiveData<Boolean> = _isOnRoomStart
+
+    private val _isOnSpark = MutableLiveData<Boolean>()
+    val isOnSpark: LiveData<Boolean> = _isOnSpark
 
     fun getAlarmSetting() {
         viewModelScope.launch {
             alarmSettingRepository.getAlarmSetting()
                 .onSuccess { response ->
-                    isOnCertification.postValue(response.data.certification)
-                    isOnConsider.postValue(response.data.consider)
-                    isOnRemind.postValue(response.data.remind)
-                    isOnRoomStart.postValue(response.data.roomStart)
-                    isOnSpark.postValue(response.data.spark)
+                    _isOnCertification.postValue(response.data.certification)
+                    _isOnConsider.postValue(response.data.consider)
+                    _isOnRemind.postValue(response.data.remind)
+                    _isOnRoomStart.postValue(response.data.roomStart)
+                    _isOnSpark.postValue(response.data.spark)
                 }.onFailure {
                     Log.d("AlarmSetting_GetAlarmSetting", it.message.toString())
                 }
         }
+    }
+
+    fun patchAlarmSetting(category: String) {
+        viewModelScope.launch {
+            alarmSettingRepository.patchAlarmSetting(category)
+                .onFailure {
+                    Log.d("AlarmSetting_PatchAlarmSetting", it.message.toString())
+                }
+        }
+    }
+
+    companion object {
+        const val ALARM_CERTIFICATION = "certification"
+        const val ALARM_CONSIDER = "consider"
+        const val ALARM_REMIND = "remind"
+        const val ALARM_ROOM_START = "roomStart"
+        const val ALARM_SPARK = "spark"
     }
 }
