dc657044a909452820d502cdf5041081605df6e8
Yunjoo Son <thsduswn4141@naver.com>
2022-03-13 16:47:12 +0900

    #326 [feat] 프로필 조회 Api 정의

---
+++ app/src/main/java/com/spark/android/data/remote/entity/response/ProfileResponse.kt
@@ -0,0 +1,8 @@
+package com.spark.android.data.remote.entity.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ProfileResponse(
+    val nickname:String,
+    val profileImg:String
+)
