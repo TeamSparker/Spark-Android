94b8ccd04ba642ea89490f1b9ec94f98dc4c218e
Yunjoo Son <thsduswn4141@naver.com>
2022-03-20 00:46:43 +0900

    #341 [layout] AlarmCenterActivity 생성

---
+++ app/src/main/java/com/spark/android/ui/alarmcenter/AlarmCenterActivity.kt
@@ -0,0 +1,13 @@
+package com.spark.android.ui.alarmcenter
+
+import android.os.Bundle
+import com.spark.android.R
+import com.spark.android.databinding.ActivityAlarmCenterBinding
+import com.spark.android.ui.base.BaseActivity
+
+class AlarmCenterActivity :
+    BaseActivity<ActivityAlarmCenterBinding>(R.layout.activity_alarm_center) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
