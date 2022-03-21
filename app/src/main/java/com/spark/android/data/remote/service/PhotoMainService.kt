1fab7f32513050b8c3ba08a9ec8d146eaf0abd48
Grandelwald <artjr97@naver.com>
2022-03-21 22:12:38 +0900

    #355 [feat] 대표이미지 선택 네트워크 서비스 생성

--- app/src/main/java/com/spark/android/data/remote/service/PhotoMainService.kt
+++ app/src/main/java/com/spark/android/data/remote/service/PhotoMainService.kt
@@ -1,10 +1,14 @@
 package com.spark.android.data.remote.service
 
+import com.spark.android.data.remote.entity.response.NoDataResponse
+import retrofit2.Call
 import retrofit2.http.PATCH
+import retrofit2.http.Path
 
 interface PhotoMainService {
-    @PATCH("")
+    @PATCH("myroom/{roomId}/{recordId}")
     fun patchPhotoMainData(
-
-    )
+        @Path("roomId") roomId: Int,
+        @Path("recordId") recordId : Int
+    ): Call<NoDataResponse>
 }
