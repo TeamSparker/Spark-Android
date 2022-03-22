26a0abb20dff1eb304cdd23ebf6b7de2a1c94352
Yunjoo Son <thsduswn4141@naver.com>
2022-03-23 01:20:09 +0900

    #342 [feat] 스파커 활동 알림 아이템에 이미지 띄우기 (피드, 프로필 사진 분기처리)

--- app/src/main/java/com/spark/android/data/remote/entity/response/AlarmResponse.kt
+++ app/src/main/java/com/spark/android/data/remote/entity/response/AlarmResponse.kt
@@ -3,18 +3,19 @@ package com.spark.android.data.remote.entity.response
 import com.google.gson.annotations.SerializedName
 
 data class AlarmResponse(
-    val newService: Boolean,
+    val newService: Boolean?,
+    val newActive: Boolean?,
     @SerializedName("notices")
     val alarms: List<Alarm>
 )
 
 data class Alarm(
+    val noticeId: Int,
+    val noticeTitle: String,
+    val noticeContent: String,
     val day: String,
     val isNew: Boolean,
-    val isThumbProfile: Boolean,
-    val noticeContent: String,
-    val noticeId: Int,
     val noticeImg: String?,
-    val noticeTitle: String,
+    val isThumbProfile: Boolean?,
     var isFirst: Boolean = false
 )
