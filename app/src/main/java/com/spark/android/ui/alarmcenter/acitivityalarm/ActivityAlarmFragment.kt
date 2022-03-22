d0b4729127c04be38c37a0336e88618b3d88c448
Yunjoo Son <thsduswn4141@naver.com>
2022-03-22 21:41:58 +0900

    #342 [feat] 스파커 활동 알림 리사이클러뷰 어댑터 설정 및 UI에 페이징된 데이터 표시

--- app/src/main/java/com/spark/android/ui/alarmcenter/acitivityalarm/ActivityAlarmFragment.kt
+++ app/src/main/java/com/spark/android/ui/alarmcenter/acitivityalarm/ActivityAlarmFragment.kt
@@ -2,15 +2,42 @@ package com.spark.android.ui.alarmcenter.acitivityalarm
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.spark.android.R
 import com.spark.android.databinding.FragmentActivityAlarmBinding
+import com.spark.android.ui.alarmcenter.acitivityalarm.adapter.ActivityAlarmPagingAdapter
+import com.spark.android.ui.alarmcenter.acitivityalarm.viewmodel.ActivityAlarmViewModel
 import com.spark.android.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ActivityAlarmFragment :
     BaseFragment<FragmentActivityAlarmBinding>(R.layout.fragment_activity_alarm) {
+    private val activityAlarmViewModel by viewModels<ActivityAlarmViewModel>()
+    private val activityAlarmAdapter = ActivityAlarmPagingAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRvActivityAlarmAdapter()
+        collectActivityAlarmList()
+    }
+
+    private fun initRvActivityAlarmAdapter() {
+        binding.rvActivity.adapter = activityAlarmAdapter
+    }
+
+    private fun collectActivityAlarmList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                activityAlarmViewModel.getActivityAlarmPagingSource().collectLatest { alarmList ->
+                    activityAlarmAdapter.submitData(alarmList)
+                }
+            }
+        }
     }
 }
