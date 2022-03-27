1a6b8fa7b98a81cd02f85c1b584b9aa6c5da3d5b
Jaehun-Jo <chojh922@gmail.com>
2022-03-27 22:24:57 +0900

    [layout] 습관방 스파크 보내기 업데이트 뷰 및 ic, shape 추가

---
+++ app/src/main/java/com/spark/android/ui/habit/HabitSendSparkActivity.kt
@@ -0,0 +1,15 @@
+package com.spark.android.ui.habit
+
+import android.os.Bundle
+import com.spark.android.R
+import com.spark.android.databinding.ActivityHabitSendSparkBinding
+import com.spark.android.ui.base.BaseActivity
+
+class HabitSendSparkActivity :
+    BaseActivity<ActivityHabitSendSparkBinding>(R.layout.activity_habit_send_spark) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
