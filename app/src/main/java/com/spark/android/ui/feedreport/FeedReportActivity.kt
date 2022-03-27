f3ab83bc6491b812e133e15a1c980531d5eb862c
Yunjoo Son <thsduswn4141@naver.com>
2022-03-27 23:18:29 +0900

    #380 [layout] 피드 신고하기 이유 입력 화면 레이아웃 제작

---
+++ app/src/main/java/com/spark/android/ui/feedreport/FeedReportActivity.kt
@@ -0,0 +1,12 @@
+package com.spark.android.ui.feedreport
+
+import android.os.Bundle
+import com.spark.android.R
+import com.spark.android.databinding.ActivityFeedReportBinding
+import com.spark.android.ui.base.BaseActivity
+
+class FeedReportActivity : BaseActivity<ActivityFeedReportBinding>(R.layout.activity_feed_report) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
