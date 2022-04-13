402116030e6ebad995be617c5425df7abb150857
Grandelwald <artjr97@naver.com>
2022-04-13 16:47:45 +0900

    #396 [feat] skip 버튼 누를시 온보딩 마지막 페이지로 이동

--- app/src/main/java/com/spark/android/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/spark/android/ui/onboarding/OnBoardingActivity.kt
@@ -3,24 +3,29 @@ package com.spark.android.ui.onboarding
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.spark.android.R
 import com.spark.android.databinding.ActivityOnBoardingBinding
 import com.spark.android.ui.auth.AuthActivity
 import com.spark.android.ui.base.BaseActivity
-import com.spark.android.ui.main.MainActivity
 import com.spark.android.ui.onboarding.adapter.OnBoardingVpAdapter
 
 class OnBoardingActivity : BaseActivity<ActivityOnBoardingBinding>(R.layout.activity_on_boarding) {
     private lateinit var onBoardingVpAdapter: OnBoardingVpAdapter
+    private lateinit var onBoardingPageList: List<Fragment>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initAdapter()
+        initOnBoardingVpAdapter()
+        initOnBoardingVpIndicator()
+        setBtnVisibilityOnLastPage()
+        initSkipBtn()
+        initStartBtn()
     }
 
-    private fun initAdapter() {
-        val onBoardingPageList = listOf(
+    private fun initOnBoardingVpAdapter() {
+        onBoardingPageList = listOf(
             OnBoardingOneFragment(),
             OnBoardingTwoFragment(),
             OnBoardingThreeFragment(),
@@ -30,23 +35,35 @@ class OnBoardingActivity : BaseActivity<ActivityOnBoardingBinding>(R.layout.acti
         onBoardingVpAdapter.fragments.addAll(onBoardingPageList)
 
         binding.vpOnBoarding.adapter = onBoardingVpAdapter
-        binding.indicatorOnBoarding.setViewPager(binding.vpOnBoarding)
+    }
 
-        binding.vpOnBoarding.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+    private fun initOnBoardingVpIndicator(){
+        binding.indicatorOnBoarding.setViewPager(binding.vpOnBoarding)
+    }
+
+    private fun setBtnVisibilityOnLastPage() {
+        binding.vpOnBoarding.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                val isLastPage = position == onBoardingPageList.size -1
-                binding.btnOnboardingSkip.visibility = if (isLastPage) View.INVISIBLE else View.VISIBLE
-                binding.btnOnboardingStart.visibility = if (!isLastPage) View.INVISIBLE else View.VISIBLE
+                val isLastPage = position == onBoardingPageList.size - 1
+                binding.btnOnboardingSkip.visibility =
+                    if (isLastPage) View.INVISIBLE else View.VISIBLE
+                binding.btnOnboardingStart.visibility =
+                    if (isLastPage) View.VISIBLE else View.INVISIBLE
             }
         })
+    }
 
+    private fun initSkipBtn() {
         binding.btnOnboardingSkip.setOnClickListener {
-            startActivity(Intent(this,AuthActivity::class.java))
-            finish()
+            binding.vpOnBoarding.currentItem = 3
         }
+    }
+
+    private fun initStartBtn() {
         binding.btnOnboardingStart.setOnClickListener {
-            startActivity(Intent(this,AuthActivity::class.java))
+            startActivity(Intent(this, AuthActivity::class.java))
             finish()
         }
     }
