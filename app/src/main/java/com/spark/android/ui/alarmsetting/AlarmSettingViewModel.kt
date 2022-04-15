fe2cc19e1132a6b2cb4196c5b50f905e8b93c5fe
Yunjoo Son <thsduswn4141@naver.com>
2022-04-15 16:41:37 +0900

    #408 [feat] 알림 설정 첫 접근 시 서버와 로컬 동기화, 이후 로그아웃/탈퇴 전까지 로컬 값 가져와 띄우도록 구현, 설정 값 변경 시 서버 통신 성공 후 로컬 값도 변경하도록 구현

--- app/src/main/java/com/spark/android/ui/alarmsetting/AlarmSettingViewModel.kt
+++ app/src/main/java/com/spark/android/ui/alarmsetting/AlarmSettingViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.spark.android.data.remote.entity.response.AlarmSettingResponse
 import com.spark.android.data.remote.repository.AlarmSettingRepository
+import com.spark.android.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,6 +17,13 @@ import kotlin.properties.Delegates
 class AlarmSettingViewModel @Inject constructor(
     private val alarmSettingRepository: AlarmSettingRepository
 ) : ViewModel() {
+    private val _isAlarmSettingLocalSaved =
+        MutableLiveData(Event(alarmSettingRepository.getAlarmSettingLocalSaved()))
+    val isAlarmSettingLocalSaved: LiveData<Event<Boolean>> = _isAlarmSettingLocalSaved
+
+    private val _alarmSettingValue = MutableLiveData(alarmSettingRepository.getAlarmSettingValue())
+    val alarmSettingValue: LiveData<AlarmSettingResponse> = _alarmSettingValue
+
     private val _isOnCertification = MutableLiveData<Boolean>()
     val isOnCertification: LiveData<Boolean> = _isOnCertification
 
@@ -31,23 +40,34 @@ class AlarmSettingViewModel @Inject constructor(
     val isOnSpark: LiveData<Boolean> = _isOnSpark
 
     fun getAlarmSetting() {
-        viewModelScope.launch {
-            alarmSettingRepository.getAlarmSetting()
-                .onSuccess { response ->
-                    _isOnCertification.postValue(response.data.certification)
-                    _isOnConsider.postValue(response.data.consider)
-                    _isOnRemind.postValue(response.data.remind)
-                    _isOnRoomStart.postValue(response.data.roomStart)
-                    _isOnSpark.postValue(response.data.spark)
-                }.onFailure {
-                    Log.d("AlarmSetting_GetAlarmSetting", it.message.toString())
-                }
+        if (!alarmSettingRepository.getAlarmSettingLocalSaved()) {
+            Log.d("aaa", "aaa")
+            viewModelScope.launch {
+                alarmSettingRepository.getAlarmSetting()
+                    .onSuccess { response ->
+                        alarmSettingRepository.saveAlarmSettingLocalSaved(true)
+                        _isAlarmSettingLocalSaved.postValue(Event(true))
+                        alarmSettingRepository.saveAlarmSettingValue(response.data)
+                        _alarmSettingValue.postValue(response.data)
+//                        _isOnCertification.postValue(response.data.certification)
+//                        _isOnConsider.postValue(response.data.consider)
+//                        _isOnRemind.postValue(response.data.remind)
+//                        _isOnRoomStart.postValue(response.data.roomStart)
+//                        _isOnSpark.postValue(response.data.spark)
+                    }.onFailure {
+                        Log.d("AlarmSetting_GetAlarmSetting", it.message.toString())
+                    }
+            }
         }
     }
 
     fun patchAlarmSetting(category: String) {
         viewModelScope.launch {
             alarmSettingRepository.patchAlarmSetting(category)
+                .onSuccess {
+                    alarmSettingRepository.patchAlarmSettingValue(category)
+                    _alarmSettingValue.postValue(alarmSettingRepository.getAlarmSettingValue())
+                }
                 .onFailure {
                     Log.d("AlarmSetting_PatchAlarmSetting", it.message.toString())
                 }
