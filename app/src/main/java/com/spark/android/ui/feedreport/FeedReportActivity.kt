4290291a4992bed23355fdf1cc19661ce78df13e
Yunjoo Son <thsduswn4141@naver.com>
2022-04-26 18:27:32 +0900

    #456 [feat] 신고하기 버튼 클릭 후 & 신고하기 중 뒤로가기 클릭 후 피드 뷰로 돌아오도록 개선

--- app/src/main/java/com/spark/android/ui/feedreport/FeedReportActivity.kt
+++ app/src/main/java/com/spark/android/ui/feedreport/FeedReportActivity.kt
@@ -1,11 +1,13 @@
 package com.spark.android.ui.feedreport
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.spark.android.R
 import com.spark.android.databinding.ActivityFeedReportBinding
 import com.spark.android.ui.base.BaseActivity
 import com.spark.android.ui.feed.FeedBottomSheet.Companion.FEED_ITEM_ID
+import com.spark.android.ui.main.MainActivity
 import com.spark.android.util.EventObserver
 import com.spark.android.util.KeyBoardUtil
 import dagger.hilt.android.AndroidEntryPoint
@@ -43,6 +45,10 @@ class FeedReportActivity : BaseActivity<ActivityFeedReportBinding>(R.layout.acti
     private fun initIsSuccessReportObserver() {
         feedReportViewModel.isSuccessReport.observe(this, EventObserver { isSuccess ->
             if (isSuccess) {
+                startActivity(Intent(this, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK and Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    putExtra(MainActivity.FROM_WHERE, FROM_FEED_REPORT_ACTIVITY)
+                })
                 finish()
             }
         })
@@ -50,7 +56,15 @@ class FeedReportActivity : BaseActivity<ActivityFeedReportBinding>(R.layout.acti
 
     private fun initBackBtnClickListener() {
         binding.btnFeedReportBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK and Intent.FLAG_ACTIVITY_CLEAR_TASK
+                putExtra(MainActivity.FROM_WHERE, FROM_FEED_REPORT_ACTIVITY)
+            })
             finish()
         }
+    }
+
+    companion object {
+        const val FROM_FEED_REPORT_ACTIVITY = "FeedReportActivity"
     }
 }
