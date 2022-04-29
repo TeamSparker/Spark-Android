1d4da92384b12d6dd9c34330592a7e8bb48e827a
Yunjoo Son <thsduswn4141@naver.com>
2022-04-29 17:40:57 +0900

    #480 [feat] GA 트래킹 추가 - 클릭(인증 후 인스타 공유하기)

--- app/src/main/java/com/spark/android/ui/share/InstaShareDialogFragment.kt
+++ app/src/main/java/com/spark/android/ui/share/InstaShareDialogFragment.kt
@@ -11,6 +11,7 @@ import com.spark.android.R
 import com.spark.android.databinding.DialogInstaShareBinding
 import com.spark.android.util.FirebaseLogUtil
 import com.spark.android.util.FirebaseLogUtil.CLICK_FEED
+import com.spark.android.util.FirebaseLogUtil.CLICK_SHARE_INSTAGRAM
 
 class InstaShareDialogFragment : DialogFragment() {
     private var _binding: DialogInstaShareBinding? = null
@@ -52,6 +53,7 @@ class InstaShareDialogFragment : DialogFragment() {
 
     private fun setConfirmTextClickListener() {
         binding.btnInstaShare.setOnClickListener {
+            FirebaseLogUtil.logClickEvent(CLICK_SHARE_INSTAGRAM)
             setFragmentResult(INSTA_DIALOG, bundleOf(SHARE_MODE to SHARE))
             dismiss()
         }
