e97a79116450d80df0246ba7e5f255dc0bdfc506
Yunjoo Son <thsduswn4141@naver.com>
2022-05-03 19:13:50 +0900

    #523 [feat] Timber 초기화

--- app/src/main/java/com/spark/android/SparkApplication.kt
+++ app/src/main/java/com/spark/android/SparkApplication.kt
@@ -5,17 +5,25 @@ import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 import com.spark.android.data.remote.LocalPreferences
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class SparkApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initTimber()
         LocalPreferences.init(this)
         // Kakao SDK 초기화
         KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
         // Kakao 키 해시 호출하기
-        Utility.getKeyHash(this)
+        Timber.tag("kakao_keyHash").d(Utility.getKeyHash(this))
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
 }
